a0227a916b796499c87d84617a9caefe97061fff
Akash Manohar J <akash@akash.im>
2017-10-02 13:33:21 +0530

    Block anon requests or particular user

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, abort, request
 from datetime import datetime
 from flask import jsonify
+from functools import wraps
 
 app = Flask(__name__)
+
+
+def throttle_request(function):
+    def wrapper():
+        user_id = request.args.get('user_id')
+        # Block anonymous requests
+        if user_id == None:
+            return abort(503, "blocked anonymous request. Please pass user_id.")
+
+        # Block a particular user_id
+        if user_id == "1":
+            abort(503, "Blocked for user")
+
+        return function()
+    return wrapper
+
 
 
 @app.route('/')
@@ -11,6 +28,7 @@ def index():
 
 
 @app.route('/api/date')
+@throttle_request
 def date():
     readable_timestamp = str(datetime.now())
     response = {'now': readable_timestamp}
