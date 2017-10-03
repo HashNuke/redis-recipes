42d3326645b0f93d5753cc7c6d5dbe79530f03fa
Akash Manohar J <akash@akash.im>
2017-10-03 09:00:02 +0530

    Add note about dummy rate limiter

--- rate_limiters/dummy.py
+++ rate_limiters/dummy.py
@@ -4,6 +4,8 @@ class DummyRateLimiter:
         self.options = kwargs
 
     def can_allow(self, scope):
+        # Returns False if scope is "1".
+        # Just a test thing. No worries.
         if scope == "1":
           return False
         return True
